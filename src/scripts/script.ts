2efdc2500713dd808aec7cf6d377d109efcf8a1f
Richard Herbert <richard.herbert1980@gmail.com>
2020-04-25 12:03:16 +0100

    make async function to send enquiry data to /files endpoint

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,9 +1,38 @@
+interface Enquiry{
+    name :string
+    email :string
+    message: string
+    newsletterChoice: Boolean
+}
+
 let submit :Node = document.querySelector('.submit');
 let inputs :NodeList = document.querySelectorAll('.newsletter-data');
 
+/**
+ * Pass in an enquiry object and send to the server using a promise based function
+ * @param anEnquiryObject - an enquiry object which follows the enquiry interface
+ */
+const sendEnquiry = async (anEnquiryObject:Enquiry) => {
+    const response = await fetch('/files', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(anEnquiryObject) // body data type must match "Content-Type" header
+    });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    let enquiryData :any = {};
+    let enquiryData :Enquiry = {
+        name: '',
+        email: '',
+        message: '',
+        newsletterChoice: false
+    };
 
     inputs.forEach((input) => {
         if ((<HTMLInputElement>input).type != 'checkbox'){
@@ -13,5 +42,5 @@ submit.addEventListener('click', (e) => {
         }
     });
 
-    console.log(enquiryData);
+    sendEnquiry(enquiryData).then((data) => console.log(data))
 })
